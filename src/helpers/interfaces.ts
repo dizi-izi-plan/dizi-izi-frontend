a7de654902697a7cf9ffe2a4a08e01e58789213e
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-08-18 15:59:17 +0200

    feat: add custom theme colors

---
+++ src/helpers/interfaces.ts
@@ -0,0 +1,12 @@
+import { Color } from '@mui/material';
+
+export interface MyGrey extends Color {
+  grey100: string;
+  grey200: string;
+  grey300: string;
+  grey400: string;
+  grey500: string;
+  grey600: string;
+  grey700: string;
+  grey800: string;
+}
