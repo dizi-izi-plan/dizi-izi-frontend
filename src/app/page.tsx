47b092fc7c853ecf50bafadaddb81b9acabe692b
Sanechek1337 <rednaxela659@gmail.com>
2023-08-24 17:01:23 +0600

    feat: create examples for default button, box button, icon button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { DevPage } from '../mui/DevPage';
+import MuiButtons from '@/mui/MuiButtons';
 
 export default function Home() {
   return (
     <>
       <DevPage />
+      <MuiButtons />
     </>
   );
 }
