752f829b429bbfa583617b806240ce1a21119b57
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-08-29 10:03:33 +0400

    corrected comments

--- src/components/LocalDatePicker/LocalDatePicker.tsx
+++ src/components/LocalDatePicker/LocalDatePicker.tsx
@@ -1,27 +1,32 @@
 'use client';
 import React from 'react';
+import 'dayjs/locale/ru';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker, DatePickerProps } from '@mui/x-date-pickers/DatePicker';
-import 'dayjs/locale/ru';
 import { InputVariant } from '@/components/Input/classNameConstants';
 
 type LocalDatePickerProps<T> = DatePickerProps<T> & {
   className: InputVariant;
 };
 
+/**
+ * @param value Dayjs | null
+ */
+
 export const LocalDatePicker: React.FC<LocalDatePickerProps<Date>> = ({
   className,
   ...props
 }) => {
-  //   const [value, setValue] = useState<Dayjs | null>(null); в стейте родительского компонента использовать эти типы данных
+  const { value, onChange } = props;
+
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru">
       <DatePicker
         label="Дата рождения"
         className={className}
-        value={props.value}
-        onChange={props.onChange}
+        value={value}
+        onChange={onChange}
       />
     </LocalizationProvider>
   );
