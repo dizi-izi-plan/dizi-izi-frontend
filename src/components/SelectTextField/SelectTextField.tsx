d8ff2b50c494206c4e0a2cb207ac3f8f4078bfb7
Izabella Chebakova <chebakovaizabella@gmail.com>
2023-09-12 18:45:05 +0400

    fixed some comments

--- src/components/SelectTextField/SelectTextField.tsx
+++ src/components/SelectTextField/SelectTextField.tsx
@@ -1,26 +1,13 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useCallback } from 'react';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import TextField, { TextFieldProps } from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 import { InputVariant } from '@/components/Input/classNameConstants';
 import { fetchCities } from '../../redux/features/cities-slice';
 
-const listOfCities = [
-  {
-    value: 'Питер',
-    label: 'Питер',
-  },
-  {
-    value: 'Нижний',
-    label: 'Нижний',
-  },
-  {
-    value: 'Ярославль',
-    label: 'Ярославль',
-  },
-];
+const listOfCities = ['Питер', 'Нижний', 'Ярославль'];
 
 type SelectTextFieldProps = TextFieldProps & {
   className: InputVariant;
@@ -29,12 +16,14 @@ type SelectTextFieldProps = TextFieldProps & {
 
 export const SelectTextField: React.FC<SelectTextFieldProps> = (props) => {
   const dispatch = useAppDispatch();
-  const apiCities = useAppSelector((state) => state.cities.entities);
+  const apiCities = useAppSelector((state) => state.cities.citiesNames);
   console.log(apiCities);
 
-  useEffect(() => {
-    dispatch(fetchCities());
-  }, [dispatch]);
+  const handleOnFocus = useCallback(() => {
+    if (!apiCities.length) {
+      dispatch(fetchCities());
+    }
+  }, [apiCities, dispatch]);
 
   return (
     <TextField
@@ -42,13 +31,14 @@ export const SelectTextField: React.FC<SelectTextFieldProps> = (props) => {
       SelectProps={{
         displayEmpty: true,
       }}
+      onFocus={handleOnFocus}
     >
       <MenuItem value="">
-        <em>{props.placeholder}</em>
+        <em>{listOfCities.length ? props.placeholder : 'Идет загрузка...'}</em>
       </MenuItem>
-      {listOfCities.map((option) => (
-        <MenuItem key={option.value} value={option.value}>
-          {option.label}
+      {listOfCities.map((city) => (
+        <MenuItem key={city} value={city}>
+          {city}
         </MenuItem>
       ))}
     </TextField>
--- src/redux/features/cities-slice.ts
+++ src/redux/features/cities-slice.ts
@@ -1,8 +1,11 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
-type CityOption = {
-  value: string;
-  label: string;
+type ErrorName = {
+  type: string;
+};
+
+type RequestError = {
+  errors: ErrorName;
 };
 
 type AreaResponse = {
@@ -16,32 +19,33 @@ type CountryResponse = {
 
 export const fetchCities = createAsyncThunk(
   'cities/fetchCities',
-  async (): Promise<CityOption[]> => {
-    const res = await fetch('https://api.hh.ru/areas/113');
-    const data: CountryResponse = await res.json();
+  async (): Promise<string[] | string> => {
+    try {
+      const res = await fetch('https://api.hh.ru/areas/113');
+      const data: CountryResponse = await res.json();
 
-    const cities: CityOption[] = [];
-    data.areas.forEach((region) => {
-      for (const city of region.areas) {
-        const newOption: CityOption = {
-          value: city.name,
-          label: city.name,
-        };
-        cities.push(newOption);
-      }
-    });
-    return cities;
+      const cities: string[] = [];
+      data.areas.forEach((region) => {
+        for (const city of region.areas) {
+          cities.push(city.name);
+        }
+      });
+      return cities;
+    } catch (err: unknown) {
+      const error = err as RequestError;
+      return error.errors.type;
+    }
   },
 );
 
 export type CitiesState = {
-  entities: CityOption[];
+  citiesNames: string[];
   loading: 'idle' | 'pending' | 'succeeded' | 'failed';
   error: string | null;
 };
 
 const initialState: CitiesState = {
-  entities: [],
+  citiesNames: [],
   loading: 'idle',
   error: null,
 };
@@ -51,12 +55,12 @@ const citiesSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(fetchCities.rejected, (state) => {
+    builder.addCase(fetchCities.rejected, (state, action) => {
       state.loading = 'failed';
-      state.error = 'something was wrong';
+      state.error = action.payload;
     });
     builder.addCase(fetchCities.fulfilled, (state, action) => {
-      state.entities = action.payload;
+      state.citiesNames = action.payload;
       state.loading = 'succeeded';
       state.error = null;
     });
