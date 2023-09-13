3ed434fdf141e75ae2bd198a7cc89e1c1485ab8e
Hanna-Mamedova <75042307+Hanna-Mamedova@users.noreply.github.com>
2023-09-13 15:04:14 +0200

    Feat/fe add landing step (#7)

--- src/mui/DevPage.tsx
+++ src/mui/DevPage.tsx
@@ -15,6 +15,8 @@ import {
   CLASS_NAMES_LABEL,
 } from '@/components/Input/classNameConstants';
 import { Cookie } from '@/components/Cookie/Cookie';
+import { Step } from '@/components/Step/Step';
+import { steps } from '@/components/Step/steps.data';
 
 export const DevPage: FC = () => {
   return (
@@ -172,6 +174,9 @@ export const DevPage: FC = () => {
         />
       </Box>
       <Cookie />
+      <Box sx={{ backgroundColor: 'secondary.main', p: 5 }}>
+        <Step title={steps[0].title} content={steps[0].content} />
+      </Box>
     </>
   );
 };
