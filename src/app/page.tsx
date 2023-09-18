dbcc410d670472f270402c5050e5417c6be114ab
Sanechek1337 <rednaxela659@gmail.com>
2023-09-18 14:46:02 +0600

    feat: add app logo and begin button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 import { Cookie } from '@/components/Cookie/Cookie';
 
 export default function Home() {
@@ -12,6 +15,33 @@ export default function Home() {
       >
         <Container maxWidth="lg">
           <Cookie />
+          <Stack flexDirection="column" alignItems="center" gap="264px">
+            <Stack
+              flexDirection="column"
+              alignItems="center"
+              gap="54px"
+              sx={{ marginTop: '142px' }}
+            >
+              <Box>
+                <Typography
+                  variant="h1"
+                  color="secondary.contrastText"
+                  sx={{ letterSpacing: '17.28px' }}
+                >
+                  DIZI IZI
+                </Typography>
+
+                <Typography
+                  variant="subtitle1"
+                  color="secondary.contrastText"
+                  sx={{ letterSpacing: '3px' }}
+                >
+                  онлайн-проектирование интерьера
+                </Typography>
+              </Box>
+              <Button variant="default">Начать</Button>
+            </Stack>
+          </Stack>
         </Container>
       </Box>
     </main>
