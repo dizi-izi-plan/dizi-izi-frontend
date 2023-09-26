06c6f0946731d978bd8bd740f0f2a0319e636c8e
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-09-26 10:15:15 +0200

    refactor: add todo

--- src/components/Forms/LoginForm/validationSchema.ts
+++ src/components/Forms/LoginForm/validationSchema.ts
@@ -1,5 +1,6 @@
 import { z } from 'zod';
 
+// TODO: ADD CORRECT VALIDATION SCHEMA AND MESSAGES!
 export const LoginFormValidation = z.object({
   login: z.string().min(5, { message: 'Must be 5 or more characters long' }),
   password: z.string(),
