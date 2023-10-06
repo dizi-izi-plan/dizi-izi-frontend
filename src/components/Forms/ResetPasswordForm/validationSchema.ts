2829bbf8c92d73a54deb31adc1889ba0a410cd67
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-10-06 22:39:16 +0200

    fix: changes after review

--- src/components/Forms/ResetPasswordForm/validationSchema.ts
+++ src/components/Forms/ResetPasswordForm/validationSchema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 // TODO: ADD CORRECT VALIDATION SCHEMA AND MESSAGES!
 export const ResetPasswordValidation = z.object({
-  email: z.string().min(5, { message: 'Must be 5 or more characters long' }),
+  password: z.string().min(5, { message: 'Must be 5 or more characters long' }),
 });
 
 export type ResetPasswordType = z.infer<typeof ResetPasswordValidation>;
