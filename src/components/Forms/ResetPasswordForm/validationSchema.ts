9b889e1fd372891240cfc86368e9ee2bb2f853b6
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-10-11 22:33:38 +0200

    feat: add enter-email and reset-password validations

--- src/components/Forms/ResetPasswordForm/validationSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-// TODO: ADD CORRECT VALIDATION SCHEMA AND MESSAGES!
-export const ResetPasswordValidation = z.object({
-  password: z.string().min(5, { message: 'Must be 5 or more characters long' }),
-});
-
-export type ResetPasswordType = z.infer<typeof ResetPasswordValidation>;
