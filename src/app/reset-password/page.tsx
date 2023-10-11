dbdedb1ead6f9979d501c14d14b43e42c4cc2631
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-10-11 22:12:02 +0200

    feat: add forms container

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,22 +1,17 @@
 import { ResetPasswordForm } from '@/components/Forms/ResetPasswordForm/ResetPasswordForm';
-import { Box, Stack } from '@mui/material';
+import { FormsContainer } from '@/containers/FormsContainer/FormsContainer';
+import { Box } from '@mui/material';
 
 const FORM_MINWIDTH = '300px';
 const FORM_MAXWIDTH = '460px';
 
-//TODO: CHANGE STACK TO REUSED COMPONENT CONTAINER
 const ResetPassword = () => {
   return (
-    <Stack
-      minHeight="calc(100vh - 90px)"
-      alignItems="center"
-      p={14}
-      sx={{ backgroundColor: 'primary.contrastText' }}
-    >
+    <FormsContainer padding={14}>
       <Box width="100%" minWidth={FORM_MINWIDTH} maxWidth={FORM_MAXWIDTH}>
         <ResetPasswordForm />
       </Box>
-    </Stack>
+    </FormsContainer>
   );
 };
 
