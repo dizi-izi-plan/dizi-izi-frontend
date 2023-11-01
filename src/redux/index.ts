fdf52c474c80fe6af54a7c0d2a539e8b940c2533
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-11-01 21:34:20 +0100

    feat: add measurementsReducer to state

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { citiesReducer } from './slices/cities-slice';
+import measurementsReducer from './slices/measurements-slice';
 
 export const store = configureStore({
   reducer: {
     cities: citiesReducer,
+    measurements: measurementsReducer,
   },
   devTools: true,
 });
