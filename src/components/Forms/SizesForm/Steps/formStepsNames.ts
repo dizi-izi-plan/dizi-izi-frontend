6eb7915a4ccdfc4e8b0a98588351c747313e432c
Hanna Kuzich <hanna.kuzich@gmail.com>
2023-11-01 21:33:09 +0100

    feat: add step1 from names

---
+++ src/components/Forms/SizesForm/Steps/formStepsNames.ts
@@ -0,0 +1,6 @@
+export const STEP1 = [
+  { name: 'firstWall', placeholder: 'Длина стены 1 в мм' },
+  { name: 'secondWall', placeholder: 'Длина стены 2 в мм' },
+  { name: 'thirdWall', placeholder: 'Длина стены 3 в мм' },
+  { name: 'forthWall', placeholder: 'Длина стены 4 в мм' },
+] as const;
