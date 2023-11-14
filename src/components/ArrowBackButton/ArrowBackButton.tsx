e80f4d4a0284826e0e6c3592b012a2b7998091b7
Alexander <rednaxela659@gmail.com>
2023-11-14 11:41:43 +0600

    feat: wrap the button with a popper, the onclick function is passed through props

--- src/components/ArrowBackButton/ArrowBackButton.tsx
+++ src/components/ArrowBackButton/ArrowBackButton.tsx
@@ -1,15 +1,31 @@
 'use client';
 
 import { useRouter } from 'next/navigation';
-import Button from '@mui/material/Button';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { PopperMessage } from '@/components/Popper/PopperMessage';
 
-export const ArrowBackButton = () => {
+interface ArrowBackButtonProps {
+  onClick?: () => void;
+}
+
+export const ArrowBackButton = ({ onClick }: ArrowBackButtonProps) => {
   const router = useRouter();
 
+  if (!onClick) {
+    onClick = () => {
+      router.back();
+    };
+  }
+
   return (
-    <Button variant="empty" sx={{ alignSelf: 'flex-start' }}>
-      <ArrowBackIcon onClick={() => router.back()} />
-    </Button>
+    <Box sx={{ alignSelf: 'flex-start', position: 'absolute' }}>
+      <PopperMessage tip="Назад">
+        <Button variant="empty" onClick={onClick}>
+          <ArrowBackIcon />
+        </Button>
+      </PopperMessage>
+    </Box>
   );
 };
