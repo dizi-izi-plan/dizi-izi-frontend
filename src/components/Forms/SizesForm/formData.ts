f7511ff5d5927e1a487b8499a78dbc7402e5b86d
Hanna Kuzich <hanna.kuzich@gmail.com>
2024-01-08 21:58:38 +0100

    feat: add step1 form data

---
+++ src/components/Forms/SizesForm/formData.ts
@@ -0,0 +1,6 @@
+export const STEP1 = [
+  { name: 'firstWall', placeholder: 'Длина стены 1 в мм', label: 'стена 1' },
+  { name: 'secondWall', placeholder: 'Длина стены 2 в мм', label: 'стена 2' },
+  { name: 'thirdWall', placeholder: 'Длина стены 3 в мм', label: 'стена 3' },
+  { name: 'forthWall', placeholder: 'Длина стены 4 в мм', label: 'стена 4' },
+];
