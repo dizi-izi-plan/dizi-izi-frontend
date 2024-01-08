5ec6aea1987aae8a8f311750d1bb1d6604dbc653
Hanna Kuzich <hanna.kuzich@gmail.com>
2024-01-08 21:58:22 +0100

    feat: add sizeForm walls default values

---
+++ src/components/Forms/SizesForm/defaultValues.ts
@@ -0,0 +1,14 @@
+import { SizesFormType } from './validation';
+
+export const walls = {
+  firstWall: '',
+  secondWall: '',
+  thirdWall: '',
+  forthWall: '',
+};
+
+export const initialStepsState: SizesFormType = {
+  walls,
+  doors: {},
+  windows: {},
+};
