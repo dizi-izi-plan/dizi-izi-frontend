18558439c0d1b27f61eac96be16883a3251e34d8
Hanna Kuzich <hanna.kuzich@gmail.com>
2024-01-18 23:41:09 +0100

    fix: remove unnecessary files from redux

--- src/redux/baseReducer.ts
+++
@@ -1,9 +0,0 @@
-import { PayloadAction } from '@reduxjs/toolkit';
-
-export const setProperty = <T>(
-  state: T,
-  action: PayloadAction<Record<string, unknown>>,
-) => ({
-  ...state,
-  ...action.payload,
-});
--- src/redux/selectors/selector.ts
+++
@@ -1,6 +0,0 @@
-import { RootState } from '..';
-
-export const selectCurrentStep = (state: RootState) =>
-  state.measurements.currentStep;
-
-export const selectWalls = (state: RootState) => state.measurements.walls;
