577f309f8505212faf47acbca1e637a842eb037c
Hanna Kuzich <hanna.kuzich@gmail.com>
2024-01-25 20:29:51 +0100

    feat: add walls value validation for equal or more than 1000

--- src/components/Forms/SizesForm/validation.ts
+++ src/components/Forms/SizesForm/validation.ts
@@ -2,6 +2,7 @@ import { z, number, string } from 'zod';
 
 export const MIN_WALLS_INPUT_LENGTH = 4;
 export const MAX_WALLS_INPUT_LENGTH = 4;
+export const MIN_WALLS_VALUE = 1000;
 
 export const ERROR_MESSAGES = {
   minWallsSizes: 'Длина стен не может быть меньше 1000мм',
@@ -9,7 +10,8 @@ export const ERROR_MESSAGES = {
 
 const wallValidation = z
   .union([number(), string()])
-  .refine((value) => `${value}`.length >= MIN_WALLS_INPUT_LENGTH);
+  .refine((value) => `${value}`.length >= MIN_WALLS_INPUT_LENGTH)
+  .refine((value) => +value >= MIN_WALLS_VALUE);
 
 export const WallsValidation = z.object({
   first: wallValidation,
