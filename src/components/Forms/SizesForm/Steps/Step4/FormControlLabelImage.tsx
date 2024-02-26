76e43d6007508d4987aafa206f8153df0db56527
Izabella Chebakova <chebakovaizabella@gmail.com>
2024-02-26 22:25:51 +0600

    fixed checkboxes checked styles

--- src/components/Forms/SizesForm/Steps/Step4/FormControlLabelImage.tsx
+++ src/components/Forms/SizesForm/Steps/Step4/FormControlLabelImage.tsx
@@ -1,7 +1,6 @@
 import FormControlLabel, {
   FormControlLabelProps,
 } from '@mui/material/FormControlLabel';
-import Box from '@mui/material/Box';
 
 type FormControlLabelImageProps = FormControlLabelProps & {
   lableMinHeight?: string;
@@ -12,44 +11,34 @@ export const FormControlLabelImage = ({
   ...props
 }: FormControlLabelImageProps) => {
   return (
-    <Box position="relative">
-      <FormControlLabel
-        sx={{
-          margin: 0,
+    <FormControlLabel
+      sx={{
+        margin: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        rowGap: '10px',
+        position: 'relative',
+        zIndex: 1,
+        '& .MuiTypography-root': {
+          maxWidth: '180px',
+          minHeight: lableMinHeight,
           display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-end',
-          alignItems: 'center',
-          rowGap: '10px',
-          position: 'relative',
-          zIndex: 1,
-          '& .MuiTypography-root': {
-            maxWidth: '180px',
-            minHeight: lableMinHeight,
-            display: 'flex',
-            alignItems: 'flex-end',
-          },
-          '&:hover + .underline': {
-            background: (theme) => theme.palette.primary.main,
-          },
-          '&:has(.Mui-checked) + .underline': {
-            background: (theme) => theme.palette.primary.dark,
-          },
-        }}
-        {...props}
-      />
-      <Box
-        className="underline"
-        position="absolute"
-        height="8px"
-        width="100%"
-        left="0"
-        bottom="0"
-        sx={{
-          background: 'transparent',
-          transition: 'background 0.3s linear',
-        }}
-      />
-    </Box>
+          alignItems: 'flex-start',
+        },
+        '&:hover .MuiTypography-root': {
+          textDecoration: 'underline 4px',
+          textDecorationColor: (theme) => theme.palette.primary.main,
+          textUnderlineOffset: '5px',
+        },
+        '&:has(.Mui-checked) .MuiTypography-root': {
+          textDecoration: 'underline 4px',
+          textDecorationColor: (theme) => theme.palette.primary.dark,
+          textUnderlineOffset: '5px',
+        },
+      }}
+      {...props}
+    />
   );
 };
