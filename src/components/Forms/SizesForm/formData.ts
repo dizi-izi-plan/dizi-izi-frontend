92624155b58a1032ea32d92ef448453159c776a9
Hanna Kuzich <hanna.kuzich@gmail.com>
2024-03-06 23:13:27 +0100

    feat: add door formData

--- src/components/Forms/SizesForm/formData.ts
+++ src/components/Forms/SizesForm/formData.ts
@@ -1,5 +1,12 @@
+import { RadioType } from '@/components/Input/RadioGroup/RadioGroupWrapper';
 import { ObjectValues } from '@/types/types';
 
+export const SIDE = {
+  left: 'left',
+  right: 'right',
+} as const;
+
+// WALLS
 export enum WALLS {
   first = 'walls.first',
   second = 'walls.second',
@@ -13,6 +20,37 @@ export const CORRESPONDING_WALLS = {
   'walls.third': WALLS.first,
   'walls.forth': WALLS.second,
 } as const;
+
+export const NEIGHBOR_WALLS: { [K in WALLS_NAMES_TYPE]: RadioType[] } = {
+  'walls.first': [
+    {
+      label: 'До стены 2',
+      value: SIDE.left,
+    },
+    { label: 'До стены 4', value: SIDE.right },
+  ],
+  'walls.second': [
+    { label: 'До стены 1', value: SIDE.right },
+    {
+      label: 'До стены 3',
+      value: SIDE.left,
+    },
+  ],
+  'walls.third': [
+    { label: 'До стены 2', value: SIDE.right },
+    {
+      label: 'До стены 4',
+      value: SIDE.left,
+    },
+  ],
+  'walls.forth': [
+    {
+      label: 'До стены 1',
+      value: SIDE.left,
+    },
+    { label: 'До стены 3', value: SIDE.right },
+  ],
+};
 
 export type WALLS_NAMES_TYPE = ObjectValues<typeof CORRESPONDING_WALLS>;
 
@@ -34,3 +72,68 @@ export const STEP1 = [
     placeholder: 'Длина стены 4 в мм',
   },
 ];
+
+// DOOR
+export const OPEN = {
+  inside: 'inside',
+  outside: 'outside',
+};
+
+export const STEP2 = {
+  wallNumber: {
+    name: 'door.wallNumber',
+    placeholder: 'Дверь на стене номер',
+    options: [
+      {
+        value: WALLS.first,
+        text: 1,
+      },
+      {
+        value: WALLS.second,
+        text: 2,
+      },
+      {
+        value: WALLS.third,
+        text: 3,
+      },
+      {
+        value: WALLS.forth,
+        text: 4,
+      },
+    ],
+  },
+  doorSize: {
+    name: 'door.size',
+    placeholder: 'Размер двери в мм',
+  },
+  fromDoorTo: {
+    name: 'door.distanceToWall',
+    placeholder: 'Расстояние от двери',
+  },
+  toWall: {
+    name: 'door.toWall',
+  },
+  openLeftRight: {
+    name: 'door.side',
+    radios: [
+      { value: SIDE.left, label: 'Открывается влево' },
+      {
+        value: SIDE.right,
+        label: 'Открывается вправо',
+      },
+    ],
+  },
+  openInsideOutside: {
+    name: 'door.open',
+    radios: [
+      {
+        value: OPEN.inside,
+        label: 'Открывается внутрь',
+      },
+      {
+        value: OPEN.outside,
+        label: 'Открывается наружу',
+      },
+    ],
+  },
+};
