74e22c6823d8ad0683c6278c48c25f4ea90c10e8
Hanna Kuzich <hanna.kuzich@gmail.com>
2024-03-14 23:10:30 +0100

    feat: add step fields

--- src/components/Measurements/data.ts
+++ src/components/Measurements/data.ts
@@ -1,11 +1,30 @@
+// TODO: add WindowsFields
+export type FieldNames = string[];
+
 export type MeasurementsDataType = {
   tabText: string;
   title: string;
+  fields: FieldNames;
 };
 
 export const MEASUREMENTS_STEPS: MeasurementsDataType[] = [
-  { tabText: '1 шаг', title: 'Обмеры помещения' },
-  { tabText: '2 шаг', title: 'Обозначение дверей' },
-  { tabText: '3 шаг', title: 'Обозначение окон и балконной двери' },
-  { tabText: '4 шаг', title: 'Выбор мебели' },
+  {
+    tabText: '1 шаг',
+    title: 'Обмеры помещения',
+    fields: ['walls.first', 'walls.second', 'walls.third', 'walls.forth'],
+  },
+  {
+    tabText: '2 шаг',
+    title: 'Обозначение дверей',
+    fields: [
+      'door.wallNumber',
+      'door.size',
+      'door.distanceToWall',
+      'door.toWall',
+      'door.side',
+      'door.open',
+    ],
+  },
+  { tabText: '3 шаг', title: 'Обозначение окон и балконной двери', fields: [] },
+  { tabText: '4 шаг', title: 'Выбор мебели', fields: [] },
 ];
