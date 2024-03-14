5307e88227ff33487255c597efb404cb6b98de5a
Hanna Kuzich <hanna.kuzich@gmail.com>
2024-03-14 23:00:19 +0100

    feat: add Door to form. change handleTabChange

--- src/components/Forms/SizesForm/SizesForm.tsx
+++ src/components/Forms/SizesForm/SizesForm.tsx
@@ -6,27 +6,33 @@ import { TabContentContainer } from '@/containers/TabContentContainer/TabContent
 import { a11yProps } from '@/containers/TabContentContainer/tabConstants';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
-import { Walls } from './Steps/Step1/Step1';
 import { Furniture } from './Steps/Step4/Step4';
+import { Walls } from './Steps/Step1';
 import { Dispatch, SetStateAction, SyntheticEvent } from 'react';
 import { SizesFormType } from './validation';
 import {
   Control,
   FieldErrors,
+  UseFormGetValues,
   UseFormHandleSubmit,
+  UseFormSetError,
   UseFormSetValue,
   UseFormWatch,
   UseFormResetField,
 } from 'react-hook-form';
+import { Door } from './Steps/Step2';
 
 type SizesFormProps = {
   currentStep: number;
+  validateStep: () => Promise<boolean>;
   setCurrentStep: Dispatch<SetStateAction<number>>;
   setValue: UseFormSetValue<SizesFormType>;
   watch: UseFormWatch<SizesFormType>;
   control: Control<SizesFormType>;
   errors?: FieldErrors<SizesFormType>;
   handleSubmit: UseFormHandleSubmit<SizesFormType>;
+  getValues: UseFormGetValues<SizesFormType>;
+  setError?: UseFormSetError<SizesFormType>;
   isValid: boolean;
   resetField: UseFormResetField<SizesFormType>;
 };
@@ -34,14 +40,21 @@ type SizesFormProps = {
 export const SizesForm = ({
   currentStep,
   setCurrentStep,
+  validateStep,
   setValue,
   watch,
   control,
   handleSubmit,
   isValid, // resetField,
+  getValues,
+  errors,
 }: SizesFormProps) => {
-  const handleTabChange = (event: SyntheticEvent, step: number) => {
-    if (isValid) setCurrentStep(step);
+  const handleTabChange = async (event: SyntheticEvent, step: number) => {
+    const isValid = await validateStep();
+
+    if (!isValid) return;
+
+    setCurrentStep(step);
   };
 
   const onSubmit = handleSubmit((data) => {
@@ -76,11 +89,17 @@ export const SizesForm = ({
             control={control}
             setValue={setValue}
             watch={watch}
-            isValid={isValid}
+            validateStep={validateStep}
           />
         </TabContentContainer>
         <TabContentContainer index={1} value={currentStep}>
-          Форма для дверей
+          <Door
+            control={control}
+            setValue={setValue}
+            watch={watch}
+            getValues={getValues}
+            errors={errors}
+          />
         </TabContentContainer>
         <TabContentContainer index={2} value={currentStep}>
           Форма для окон
