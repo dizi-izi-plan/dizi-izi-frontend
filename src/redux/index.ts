c9f853bf68ca7f7dc8d8ff919cefc34d4c70e2f9
Hanna Kuzich <hanna.kuzich@gmail.com>
2024-03-27 17:30:09 +0100

    feat: add current slice

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { citiesReducer } from './slices/cities-slice';
 import { fieldOnFocusReducer } from './slices/focusedFields-slice';
+import current from './slices/current-slice';
 
 export const store = configureStore({
   reducer: {
     cities: citiesReducer,
     fieldOnFocus: fieldOnFocusReducer,
+    current,
   },
   devTools: true,
 });
