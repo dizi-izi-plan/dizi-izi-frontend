8e6de5b156074649f1ec63fe157f7fe6086d4061
Bolynskii Ewgenii <119881162+MrEwgenius@users.noreply.github.com>
2024-07-30 21:10:47 +0300

    eddit

--- src/containers/DraftContainer/DraftContainer.tsx
+++ src/containers/DraftContainer/DraftContainer.tsx
@@ -8,90 +8,74 @@ import { theme } from '@/mui/theme';
 import { MenuOptionsDraft } from '@/components/Menu/MenuOptionsProjects/MenuOptionsDraft';
 import DraftIcon from '../../../public/assets/icons/draftIcon.svg';
 
-
-
-
 type DraftContainerProps = {
-
-    id: number;
-    date: string;
-    handleDelete: (id: number) => void;
-    handleContinue: (id: number) => void;
-}
+  id: number;
+  date: string;
+  handleDelete: (id: number) => void;
+  handleContinue: (id: number) => void;
+};
 
 export const DraftContainer: FC<DraftContainerProps> = ({
-    id,
-    date,
-    handleDelete,
-    handleContinue
+  id,
+  date,
+  handleDelete,
+  handleContinue,
 }) => {
+  const router = useRouter();
 
-    const router = useRouter();
+  const [, setIsOpen] = useState(false);
 
-    const [isOpen, setIsOpen] = useState(false);
+  const handleConfirm = () => {
+    router.push('/room-selection');
+    setIsOpen(false);
+  };
 
-    const handleConfirm = () => {
-        router.push('/room-selection');
-        setIsOpen(false);
-    };
-
-
-
-    return (
-        <Stack
-            height='244px'
-            width='256px'
+  return (
+    <Stack height="244px" width="256px">
+      <Button
+        variant="box"
+        size="small"
+        onClick={() => handleConfirm()}
+        startIcon={<DraftIcon />}
+      ></Button>
+      <Stack
+        alignItems="start"
+        padding="8px 12px"
+        border={`1px solid ${theme.palette.primary.main}`}
+      >
+        <Typography
+          variant="body1"
+          color={'secondary.main'}
+          onClick={() => {}}
+          sx={{
+            cursor: 'pointer',
+            border: '1px solid transparent',
+          }}
         >
-            <Button
-            
-                variant='box'
-                size="small"
-                onClick={() => handleConfirm()}
-                startIcon={
-                    <DraftIcon />
-                }
-            >
-            </Button>
-            <Stack
-                alignItems='start'
-                padding='8px 12px'
-                border={`1px solid ${theme.palette.primary.main}`}
-            >
-                <Typography
-                    variant='body1'
-                    color={'secondary.main'}
-                    onClick={() => { }}
-                    sx={{
-                        cursor: 'pointer',
-                        border: '1px solid transparent'
-                    }}
-                >
-                    {'Черновик'}
-                </Typography>
-                <Box
-                    width={'100%'}
-                    display={'flex'}
-                    alignItems={'center '}
-                    justifyContent={'space-between'}
-                >
-                    <Typography
-                        variant='overline'
-                        color={'secondary.main'}
-                        sx={{
-                            opacity: '0.6'
-                        }}
-                    >
-                        {date ? date : 'No Date'}
-                    </Typography>
+          {'Черновик'}
+        </Typography>
+        <Box
+          width={'100%'}
+          display={'flex'}
+          alignItems={'center '}
+          justifyContent={'space-between'}
+        >
+          <Typography
+            variant="overline"
+            color={'secondary.main'}
+            sx={{
+              opacity: '0.6',
+            }}
+          >
+            {date ? date : 'No Date'}
+          </Typography>
 
-                    <MenuOptionsDraft
-                        onDuplicate={() => handleContinue(id)}
-                        onDelete={() => handleDelete(id)}
-                    />
-
-                </Box>
-
-            </Stack>
-        </Stack>
-    );
+          <MenuOptionsDraft
+            onDuplicate={() => handleContinue(id)}
+            onDelete={() => handleDelete(id)}
+          />
+        </Box>
+      </Stack>
+    </Stack>
+  );
 };
