c2fe045bda17e634aaa785456e7315628ddc6445
Alexandr <rednaxela659@gmail.com>
2024-08-01 17:59:22 +0500

    refactor: delete email from message text

--- src/app/registration-letter-message/page.tsx
+++ src/app/registration-letter-message/page.tsx
@@ -1,11 +1,9 @@
 import { MessageContainer } from '@/containers/MessageContainer/MessageContainer';
 
 const RegistrationLetterMessage = () => {
-  const email = 'hhh1133@mail.ru'; // перекинуть сюда email со страницы регистрации
-
   const text = [
     `Поздравляем с регистрацией! Осталось совсем чуть-чуть.`,
-    `Мы отправили письмо на почту ${email}.
+    `Мы отправили письмо на вашу почту.
     Перейдите по ссылке из письма, чтобы подтвердить свой аккаунт.`,
   ];
 
