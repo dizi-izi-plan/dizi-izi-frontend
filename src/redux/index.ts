f68f4fd52e8794d0dbcbb94ca574815a0e09ce77
Alexandr <rednaxela659@gmail.com>
2024-08-01 12:18:36 +0500

    feat: create registration slice and add it to store

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,13 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import { citiesReducer } from './slices/cities-slice';
 import { fieldOnFocusReducer } from './slices/focusedFields-slice';
 import current from './slices/current-slice';
+import { authApi } from '@/api/apiSlice';
 
 export const store = configureStore({
   reducer: {
     cities: citiesReducer,
     fieldOnFocus: fieldOnFocusReducer,
     current,
+    [authApi.reducerPath]: authApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(authApi.middleware),
   devTools: true,
 });
 
