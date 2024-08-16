03d75d6b47186c4bc84c2e9f77a3ba3eeeaaec3e
Alexandr <rednaxela659@gmail.com>
2024-08-16 11:35:02 +0500

    feat: add api slice

---
+++ src/api/apiSlice.ts
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+type registrationDataType = {
+  email: string;
+  password: string;
+  re_password: string;
+};
+
+type registrationResponseType = {
+  id: string;
+  email: string;
+};
+
+export const authApi = createApi({
+  reducerPath: 'authApi',
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://diziizi.ru/api/v1/' }),
+  endpoints: (builder) => ({
+    getUserData: builder.query({
+      query: () => '/auth/users/',
+    }),
+    registration: builder.mutation<
+      registrationResponseType,
+      registrationDataType
+    >({
+      query: (body) => ({
+        url: 'auth/users/',
+        method: 'POST',
+        body,
+      }),
+    }),
+  }),
+});
+
+export const { useRegistrationMutation, useGetUserDataQuery } = authApi;
