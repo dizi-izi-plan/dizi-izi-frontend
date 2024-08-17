7939cd41b4edee9220efc183377bd88ebbaeee28
kit <zaurkushu@yandex.ru>
2024-08-17 18:12:49 +0300

    feat: ready-project
    add new modal logic / add new data file / add new types

--- src/components/ProjectNavbar/ProjectNavbarDataTypes.ts
+++ src/components/ProjectNavbar/ProjectNavbarDataTypes.ts
@@ -18,7 +18,22 @@ export interface ActionConfigs {
   handleActionClick: () => void;
 }
 
-export interface ModalDataProps {
-  text: string[];
+export interface ProjectButtons {
+  action: string;
+  img: string;
+  alt: string;
+  fn: () => void;
+}
+
+export interface ModalConfigs {
+  modalName: string;
+  text: Array<string>;
   handleYes: () => void;
+  handleNo: () => void;
+}
+
+export interface ActionConfigs {
+  img: string;
+  title: string;
+  fn: () => void;
 }
