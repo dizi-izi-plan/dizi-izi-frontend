c49439c9744b2e51b25b73790917f5b2004ff107
kit <zaurkushu@yandex.ru>
2024-08-17 18:12:49 +0300

    feat: ready-project
    add new modal logic / add new data file / add new types

--- src/components/ProjectNavbar/projectNavbar.data.ts
+++ src/components/ProjectNavbar/projectNavbar.data.ts
@@ -1,20 +1,82 @@
-import { ModalDataProps } from './ProjectNavbarDataTypes';
+import { modalNames } from '@/helpers/common-constants/modal-constants';
+import {
+  ActionConfigs,
+  ModalConfigs,
+  ProjectButtons,
+} from './ProjectNavbarDataTypes';
+import { setCurrentModal } from '@/redux/slices/modal-slice';
+import { Dispatch } from '@reduxjs/toolkit';
 
-export const modalData: { [key: string]: ModalDataProps } = {
-  tryAgain: {
+export const modalConfigs: Array<ModalConfigs> = [
+  {
+    modalName: modalNames.modalTryAgain,
     text: ['Вы уверены, что хотите попробовать еще раз?'],
-    handleYes: () => console.log('Try Again: Yes'),
+    handleYes: () => console.log('Yes'),
+    handleNo: () => console.log('No'),
   },
-  newProject: {
-    text: ['Проект не сохранен. Продолжить?'],
-    handleYes: () => console.log('New Project: Yes'),
+  {
+    modalName: modalNames.modalStartNewProject,
+    text: ['Проект не сохранён. Продолжить?'],
+    handleYes: () => console.log('Yes'),
+    handleNo: () => console.log('No'),
   },
-  deleteProject: {
+  {
+    modalName: modalNames.modalDeleteProject,
     text: ['Вы уверены, что хотите удалить проект?'],
-    handleYes: () => console.log('Delete Project: Yes'),
+    handleYes: () => console.log('Yes'),
+    handleNo: () => console.log('No'),
   },
-  moreActions: {
-    text: ['Что вы хотите сделать дальше?'],
-    handleYes: () => console.log('More Actions: Yes'),
+];
+export const actionConfigs: Array<ActionConfigs> = [
+  {
+    img: '../../../assets/icons/downloadPdfIcon.svg',
+    title: 'Скачать в pdf',
+    fn: () => {
+      console.log('Сработало');
+    },
   },
-};
+  {
+    img: '../../../assets/icons/sendMailIcon.svg',
+    title: 'Отправить на почту',
+    fn: () => {
+      console.log('Сработало');
+    },
+  },
+  {
+    img: '../../../assets/icons/saveProjectIcon.svg',
+    title: 'Сохранить',
+    fn: () => {
+      console.log('Сработало');
+    },
+  },
+];
+
+export const getProjectButtons = (
+  dispatch: Dispatch,
+  toggleActionBlock: () => void,
+): Array<ProjectButtons> => [
+  {
+    action: 'Попробовать еще',
+    img: 'assets/icons/refresh.svg',
+    alt: 'Refresh Icon',
+    fn: () => dispatch(setCurrentModal(modalNames.modalTryAgain)),
+  },
+  {
+    action: 'Новый проект',
+    img: 'assets/icons/add.svg',
+    alt: 'New Project Icon',
+    fn: () => dispatch(setCurrentModal(modalNames.modalStartNewProject)),
+  },
+  {
+    action: 'Удалить проект',
+    img: 'assets/icons/delete.svg',
+    alt: 'Delete Project Icon',
+    fn: () => dispatch(setCurrentModal(modalNames.modalDeleteProject)),
+  },
+  {
+    action: '',
+    img: 'assets/icons/threeDots.svg',
+    alt: 'More actions Icon',
+    fn: () => toggleActionBlock(),
+  },
+];
