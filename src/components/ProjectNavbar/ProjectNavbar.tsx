15399db5ffca9c85c3e138d89d15a490116be417
kit <zaurkushu@yandex.ru>
2024-08-17 18:12:49 +0300

    feat: ready-project
    add new modal logic / add new data file / add new types

--- src/components/ProjectNavbar/ProjectNavbar.tsx
+++ src/components/ProjectNavbar/ProjectNavbar.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { useAppDispatch } from '@/redux/hooks';
 import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
 import { Box, Button, Typography } from '@mui/material';
 import ModalIcon from '@public/assets/icons/modal_icon.svg';
--- src/components/ProjectNavbar/ProjectNavbarDataTypes.ts
+++ src/components/ProjectNavbar/ProjectNavbarDataTypes.ts
@@ -23,7 +23,22 @@ export interface ModalDataProps {
   handleYes: () => void;
 }
 
-export interface ModalDataProps {
-  text: string[];
+export interface ProjectButtons {
+  action: string;
+  img: string;
+  alt: string;
+  fn: () => void;
+}
+
+export interface ModalConfigs {
+  modalName: string;
+  text: Array<string>;
   handleYes: () => void;
+  handleNo: () => void;
+}
+
+export interface ActionConfigs {
+  img: string;
+  title: string;
+  fn: () => void;
 }
