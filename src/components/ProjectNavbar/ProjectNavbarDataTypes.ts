aa21daa88c4269708309e21ea63a4a539cc751a8
kit <zaurkushu@yandex.ru>
2024-10-20 22:12:40 +0300

    fix/merge conflicts

--- src/components/ProjectNavbar/ProjectNavbarDataTypes.ts
+++ src/components/ProjectNavbar/ProjectNavbarDataTypes.ts
@@ -22,28 +22,3 @@ export interface ModalDataProps {
   text: string[];
   handleYes: () => void;
 }
-
-export interface ProjectButtons {
-  action: string;
-  img: string;
-  alt: string;
-  fn: () => void;
-}
-
-export interface ModalConfigs {
-  modalName: string;
-  text: Array<string>;
-  handleYes: () => void;
-  handleNo: () => void;
-}
-
-export interface ActionConfigs {
-  img: string;
-  title: string;
-  fn: () => void;
-}
-
-export interface ModalDataProps {
-  text: string[];
-  handleYes: () => void;
-}
