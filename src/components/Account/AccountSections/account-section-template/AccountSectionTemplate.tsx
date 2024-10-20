21a28056f3d0f2a39720a244f9d4c145b64da108
kit <zaurkushu@yandex.ru>
2024-10-20 21:56:50 +0300

    feat: account

--- src/components/Account/AccountSections/account-section-template/AccountSectionTemplate.tsx
+++ src/components/Account/AccountSections/account-section-template/AccountSectionTemplate.tsx
@@ -1,24 +1,12 @@
 import { ProfileDataForm } from '@/components/Forms/ProfileDataForm/ProfileDataForm';
-import { useDeleteUserMutation } from '@/redux/slices/deleteUser-slice';
 import { Box, Button, Stack } from '@mui/material';
 
 export const AccountSectionTemplate = () => {
-  const [deleteUser] = useDeleteUserMutation();
-
-  const handleDelete = async () => {
-    try {
-      await deleteUser({}).unwrap();
-      console.log('User deleted');
-    } catch (error) {
-      console.error('Failed to delete user:', error);
-    }
-  };
-
   return (
     <Stack>
       <ProfileDataForm />
       <Box paddingTop={18}>
-        <Button color="secondary" onClick={handleDelete}>
+        <Button color="secondary" onClick={() => console.log('Delete user')}>
           Удалить профиль
         </Button>
       </Box>
--- src/helpers/common-constants/urls-constants.ts
+++ src/helpers/common-constants/urls-constants.ts
@@ -6,6 +6,5 @@ export const urls = {
     activateUserUrl: 'auth/users/activation/',
     resendActivationUrl: 'auth/users/resend_activation/',
     getUserDataUrl: 'auth/users/me/',
-    deleteUserUrl: 'auth/users/me/',
   },
 };
