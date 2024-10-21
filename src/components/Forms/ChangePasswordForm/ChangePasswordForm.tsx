e7992d043e61aabf727006a4cbbb13885defacaa
kit <zaurkushu@yandex.ru>
2024-10-21 14:45:57 +0300

    feat: myProfile

--- src/components/Forms/ChangePasswordForm/ChangePasswordForm.tsx
+++ src/components/Forms/ChangePasswordForm/ChangePasswordForm.tsx
@@ -28,12 +28,12 @@ export const ChangePasswordForm = () => {
   const passwordConfig = getPasswordConfig(control, errors);
 
   return (
-    <Stack spacing={5} display={'flex'} alignItems={'left'}>
+    <Stack spacing={5}>
       <Typography fontSize={32} fontWeight={500}>
         Смена пароля
       </Typography>
       <form onSubmit={onSubmit}>
-        <Stack spacing={4} width={'25rem'} display={'flex'}>
+        <Stack spacing={4} width={'25rem'}>
           {passwordConfig.map(
             ({
               name,
